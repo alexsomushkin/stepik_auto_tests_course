1861ca9fb60af3c8441b5329c2a0f32851dfaeab
unknown <you@example.comalex.somushkin@gmail.com>
2023-07-21 15:56:16 +0200

    added hello.py file

---
+++ hello.py
@@ -0,0 +1,10 @@
+from tracemalloc import stop
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+browser = webdriver.Chrome()
+browser.get("pornhub.com")
+browser.get("redtube.com")
+print("sasi sasi")
+
